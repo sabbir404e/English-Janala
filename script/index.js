23634610f3ed686fdb001278293d51b8dc9dd538
MD. Sabbir Hossain <sabbirhossain8721@gmail.com>
2026-03-08 02:56:09 +0600

    volume update

--- script/index.js
+++ script/index.js
@@ -4,6 +4,12 @@ const createElements = (arr) => {
     return (htmlElements.join(" "));
 };
 
+function pronounceWord(word) {
+  const utterance = new SpeechSynthesisUtterance(word);
+  utterance.lang = "en-EN"; // English
+  window.speechSynthesis.speak(utterance);
+}
+
 
 const manageSpinner = (status) => {
 
@@ -121,7 +127,7 @@ const displayLevelWord = (words) => {
                
                <button onclick="loadWordDetail(${word.id})" class="btn bg-[#1A91FF10] hover:bg-[#1A91FF80]"><i class="fa-solid fa-circle-info"></i></button>
 
-               <button class="btn bg-[#1A91FF10] hover:bg-[#1A91FF80]"><i class="fa-solid fa-volume-high"></i></button>
+               <button onclick="pronounceWord('${word.word}')" class="btn bg-[#1A91FF10] hover:bg-[#1A91FF80]"><i class="fa-solid fa-volume-high"></i></button>
 
             </div>
        </div>
